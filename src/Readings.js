f9c29a2d4cc56ff91535c7979ebad59df32f81e2
arnoldpe0 <arnoldpe0@gmail.com>
2019-10-25 14:35:47 -0400

    mraow

--- src/Readings.js
+++ src/Readings.js
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 import { withFirebase } from "./components/Firebase";
 import { makeStyles } from "@material-ui/core/styles";
 import MaterialTable from "material-table";
+import { Sunburst } from "react-vis";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import LinearProgress from "@material-ui/core/LinearProgress";
+
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles(theme => ({
-  root: {}
+  root: {
+    flexGrow: 1
+  }
 }));
 
 class Readings extends Component {
@@ -38,12 +46,66 @@ class Readings extends Component {
 
     return (
       <div>
-        {loading && <div>Loading ...</div>}
-        <ReadingList readings={readings} />
+        {loading ? (
+          <Container maxWidth="sm">
+            <Box my={4}>
+              <LinearProgress />
+            </Box>
+          </Container>
+        ) : (
+          <React.Fragment>
+            <ReadingVis readings={readings} />
+            <ReadingList readings={readings} />
+          </React.Fragment>
+        )}
       </div>
     );
   }
 }
+
+const ReadingVis = ({ readings }) => {
+  const classes = useStyles();
+  const data = {
+    title: "analytics",
+    color: "#12939A",
+    children: [
+      {
+        title: "cluster",
+        children: [
+          { title: "AgglomerativeCluster", color: "#12939A", size: 3938 },
+          { title: "CommunityStructure", color: "#12939A", size: 3812 },
+          { title: "HierarchicalCluster", color: "#12939A", size: 6714 },
+          { title: "MergeEdge", color: "#12939A", size: 743 }
+        ]
+      },
+      {
+        title: "graph",
+        children: [
+          { title: "BetweennessCentrality", color: "#12939A", size: 3534 },
+          { title: "LinkDistance", color: "#12939A", size: 5731 },
+          { title: "MaxFlowMinCut", color: "#12939A", size: 7840 },
+          { title: "ShortestPaths", color: "#12939A", size: 5914 },
+          { title: "SpanningTree", color: "#12939A", size: 3416 }
+        ]
+      },
+      {
+        title: "optimization",
+        children: [{ title: "AspectRatioBanker", color: "#12939A", size: 7074 }]
+      }
+    ]
+  };
+  return (
+    <React.Fragment>
+      <Sunburst
+        hideRootNode
+        colorType="literal"
+        data={data}
+        height={350}
+        width={350}
+      />
+    </React.Fragment>
+  );
+};
 
 const ReadingList = ({ readings }) => {
   const classes = useStyles();
@@ -59,9 +121,11 @@ const ReadingList = ({ readings }) => {
             { title: "UID", field: "uid" }
           ]}
           data={readings}
+          options={{
+            exportButton: true
+          }}
           title="Elem Confuzorb"
         />
-        {console.log(readings)}
       </div>
     </React.Fragment>
   );
